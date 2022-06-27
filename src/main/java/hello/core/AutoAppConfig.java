c6a988097dfa7c4ac090af832481238d2f2ca517
dundegi <sinpno@naver.com>
2022-06-27 18:14:13 +0900

    filterScan

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member", //필요한 위치부터 탐색 시작
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
 

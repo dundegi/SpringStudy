6b72c7794dd50e8841692aee9f38f00b49105040
dundegi <sinpno@naver.com>
2022-07-06 17:40:07 +0900

    request scope

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -48,7 +49,7 @@ public class SingletonWithPrototypeTest1 {
         private Provider<PrototypeBean> prototypeBeanObjectProvider;
 
         public int logic(){
-            PrototypeBean prototypeBean = prototypeBeanObjectProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanObjectProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }

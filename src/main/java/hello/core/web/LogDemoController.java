808b68efb0133d007b1652de4e543f51e418371b
dundegi <sinpno@naver.com>
2022-07-06 18:31:03 +0900

    proxy

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,15 +14,16 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
 
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
+
+        System.out.println("myLogger = " + myLogger.getClass());
 
         myLogger.log("Controller test");
         logDemoService.logic("testID");
